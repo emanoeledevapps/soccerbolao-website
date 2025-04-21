7f1f686c5af288fc8f5e70dd46a9eae174ee8d62
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-04-21 17:02:59 -0300

    feat: add logo header and image hero

--- app/(home)/components/HeroHome.tsx
+++ app/(home)/components/HeroHome.tsx
@@ -1,21 +1,32 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import ImageHero from '@/public/images/image-hero-home.png'
 
 export function HeroHome() {
   return (
-    <section className="flex flex-col items-center gap-5 pt-20 container mx-auto">
-      <h2 className="font-bold text-5xl text-white text-center">
-        <span className="text-green-1">Diversão de verdade</span>
-        <br/>
-        para quem ama futebol
-      </h2>
+    <section className="flex flex-wrap items-center justify-center gap-32 pt-20 container mx-auto">
+      <div className="flex flex-col gap-5">
+        <h2 className="font-bold text-5xl text-white">
+          <span className="text-green-1">Diversão de verdade</span>
+          <br />
+          para quem ama futebol
+        </h2>
 
-      <p className="text-center text-gray-p max-w-[500px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-    
-      <div className="w-[80%] h-[300px] bg-red-500 mt-5"/>
+        <p className="text-gray-p max-w-[500px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+      
+        <Button className="w-[400px] rounded-2xl mt-5">
+          Baixe agora nosso APP
+        </Button>
+      </div>
 
-      <Button className="w-[400px] rounded-2xl mt-5">
-        Baixe agora nosso APP
-      </Button>
+      <Image
+        alt="dois screenshots mostrando o app soccer bolão"
+        width={500}
+        height={500}
+        src={ImageHero}
+        className="w-[400px] h-[500px] object-contain"
+        quality={100}
+      />
     </section>
   )
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,20 @@
+import Image  from "next/image";
 import { Button } from "../ui/button";
+import Logo from '@/public/images/logo.png'
 
 export function Header() {
   return (
-    <header className="h-16 w-screen bg-gray-1">
+    <header className="h-20 w-screen bg-gray-1">
       <div className="container mx-auto px-10 flex items-center justify-between h-full">
-        <h1 className="font-bold text-white">Soccer Bolão</h1>
+        <div className="flex items-center gap-3">
+          <Image
+            src={Logo}
+            alt="logo do app soccer bolão"
+            className="w-14 h-14 object-contain"
+          />
+
+          <h1 className="font-bold text-white">Soccer Bolão</h1>
+        </div>
 
         <Button>
           Download APP
