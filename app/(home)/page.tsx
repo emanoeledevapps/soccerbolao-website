9a6ecacb3928760fb5947f1b72ff6400632a2891
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-04-21 12:34:21 -0300

    feat: update next to version 15 and migrate pages router to app router

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'My Page Title',
+}
+
+export default function Home() {
+  return (
+    <div>
+      home
+    </div>
+  )
+}
