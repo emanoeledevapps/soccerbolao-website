5439df54dfdefb606d3ca931eb5689f9f932f1fc
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-04-21 17:39:18 -0300

    feat: initial responsive to mobile

--- app/(home)/components/HeroHome.tsx
+++ app/(home)/components/HeroHome.tsx
@@ -4,17 +4,17 @@ import ImageHero from '@/public/images/image-hero-home.png'
 
 export function HeroHome() {
   return (
-    <section className="flex flex-wrap items-center justify-center gap-32 pt-20 container mx-auto">
+    <section className="flex flex-wrap items-center justify-center px-5 pt-20 md:pt-10 md:gap-20 lg:gap-32 container mx-auto">
       <div className="flex flex-col gap-5">
-        <h2 className="font-bold text-5xl text-white">
+        <h2 className="font-bold text-3xl text-white text-center md:text-left md:text-3xl">
           <span className="text-green-1">Diversão de verdade</span>
           <br />
           para quem ama futebol
         </h2>
 
-        <p className="text-gray-p max-w-[500px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-      
-        <Button className="w-[400px] rounded-2xl mt-5">
+        <p className="text-gray-p max-w-[500px] text-center md:text-left">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+        <Button className="hidden md:flex w-[400px] rounded-2xl mt-5">
           Baixe agora nosso APP
         </Button>
       </div>
@@ -24,9 +24,13 @@ export function HeroHome() {
         width={500}
         height={500}
         src={ImageHero}
-        className="w-[400px] h-[500px] object-contain"
+        className="w-full ml-10 h-[400px] md:ml-0 md:w-[400px] md:h-[500px] object-contain"
         quality={100}
       />
+
+      <Button className="md:hidden w-full rounded-2xl">
+        Baixe agora nosso APP
+      </Button>
     </section>
   )
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,18 +5,18 @@ import Logo from '@/public/images/logo.png'
 export function Header() {
   return (
     <header className="h-20 w-screen bg-gray-1">
-      <div className="container mx-auto px-10 flex items-center justify-between h-full">
+      <div className="container mx-auto px-5 flex items-center justify-between h-full md:px-10">
         <div className="flex items-center gap-3">
           <Image
             src={Logo}
             alt="logo do app soccer bolão"
-            className="w-14 h-14 object-contain"
+            className="w-10 h-10 object-contain md:w-14 md:h-14"
           />
 
-          <h1 className="font-bold text-white">Soccer Bolão</h1>
+          <h1 className="font-bold max-w-[50%] text-white text-sm md:text-base md:max-w-full">Soccer Bolão</h1>
         </div>
 
-        <Button>
+        <Button className="text-xs md:text-base">
           Download APP
         </Button>
       </div>
