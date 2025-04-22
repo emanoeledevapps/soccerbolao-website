c6b9d36ee6adfd775ea63423952b04598f7e6a08
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-04-21 22:05:29 -0300

    feat: add terms pages

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,62 @@
+import type { Metadata } from 'next'
+import { Header } from '@/components/Header/Header'
+import { Footer } from '@/components/Footer/Footer'
+
+export const metadata: Metadata = {
+  title: 'My Page Title',
+}
+
+
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <Header />
+
+      <main className="my-20 container mx-auto px-5 md:px-10 lg:px-32">
+        <h2 className="font-bold text-white text-center text-4xl">Política de privacidade</h2>
+
+        <p className="mt-10">Última atualização: 25 de março de 2025</p>
+        <h3 className="mt-10">A sua privacidade é importante para nós. Esta Política de Privacidade tem como objetivo explicar como coletamos, usamos, protegemos e compartilhamos seus dados pessoais ao usar o aplicativo Soccer Bolão.</h3>
+
+        <h3 className="font-bold mt-10 mb-3">1. Informações que Coletamos</h3>
+        <p className="">Ao utilizar o aplicativo, coletamos os seguintes dados pessoais:</p>
+        <p className="ml-5">- Nome completo: Para identificar você dentro do aplicativo;</p>
+        <p className="ml-5">- Email: Para enviar notificações, atualizações e comunicação sobre sua conta;</p>
+        <p className="ml-5">- Foto de perfil (opcional): Para personalizar sua conta;</p>
+        <p className="ml-5">- Nome de usuário: Criado durante o cadastro, utilizado para sua identificação no aplicativo.</p>
+        <p className="">Não coletamos dados financeiros ou de pagamento, como número de cartão de crédito ou dados bancários. Toda a gestão de pagamentos de assinaturas é realizada diretamente pelo Google Payments, que é responsável por processar e gerenciar suas transações financeiras.</p>
+
+        <h3 className="font-bold mt-10 mb-3">2. Uso dos Dados</h3>
+        <p className="">Os dados coletados são utilizados para as seguintes finalidades:</p>
+        <p className="ml-5">- Cadastro e autenticação: Criar e gerenciar sua conta no aplicativo;</p>
+        <p className="ml-5">- Personalização de conteúdo: Ajustar a experiência do usuário com base nas preferências, como foto de perfil e nome de usuário;</p>
+        <p className="ml-5">- Notificações e comunicações: Enviar atualizações sobre o aplicativo, como novos recursos, alterações nos termos de uso ou informações relacionadas à sua conta;</p>
+        <p className="ml-5">- Melhoria do aplicativo: Analisar dados de uso para aprimorar a funcionalidade e a experiência do aplicativo.</p>
+
+        <h3 className="font-bold mt-10 mb-3">3. Exibição de Anúncios</h3>
+        <p className="">O aplicativo pode exibir anúncios de terceiros. Esses anúncios não são de responsabilidade do Soccer Bolão, e a exibição de anúncios está sujeita à política de privacidade e aos termos de uso dos serviços de publicidade de terceiros. O Soccer Bolão não coleta dados relacionados à exibição de anúncios e não tem controle sobre o conteúdo desses anúncios.</p>
+
+        <h3 className="font-bold mt-10 mb-3">4. Compartilhamento de Dados</h3>
+        <p className="">Não compartilhamos seus dados pessoais com terceiros, exceto quando necessário para cumprir obrigações legais, regulamentares ou quando exigido por autoridades competentes.</p>
+
+        <h3 className="font-bold mt-10 mb-3">5. Segurança dos Dados</h3>
+        <p className="">Adotamos medidas de segurança técnicas e administrativas para proteger seus dados pessoais contra acessos não autorizados, vazamentos, alterações e destruição. No entanto, como qualquer sistema online, não podemos garantir a segurança absoluta.</p>
+
+        <h3 className="font-bold mt-10 mb-3">6. Direitos dos Usuários (LGPD)</h3>
+        <p className="">Em conformidade com a Lei Geral de Proteção de Dados (LGPD), você tem os seguintes direitos:</p>
+        <p className="ml-5">- Acesso aos dados: Você pode acessar, corrigir ou excluir seus dados pessoais a qualquer momento;</p>
+        <p className="ml-5">- Revogação do consentimento: Você pode revogar seu consentimento para o processamento de seus dados pessoais a qualquer momento;</p>
+        <p className="ml-5">- Portabilidade de dados: Você pode solicitar a portabilidade dos seus dados pessoais a outro provedor de serviços, se aplicável;</p>
+        <p className="">Para exercer seus direitos, entre em contato conosco através do nosso e-mail: contato@soccerbolao.com.br</p>
+
+        <h3 className="font-bold mt-10 mb-3">7. Não é um Aplicativo de Apostas</h3>
+        <p className="">O Soccer Bolão não é um aplicativo de apostas esportivas. O objetivo do aplicativo é entretenimento e interação social, permitindo que usuários criem e participem de bolões e ligas de futebol com amigos ou outros usuários, sem envolver questões financeiras ou apostas.</p>
+
+        <h3 className="font-bold mt-10 mb-3">8. Alterações na Política de Privacidade</h3>
+        <p className="">Podemos atualizar esta Política de Privacidade periodicamente para refletir mudanças em nossas práticas de dados. Quaisquer alterações serão publicadas nesta página e, se as alterações forem significativas, notificaremos você por e-mail ou por meio de uma notificação no aplicativo.</p>
+      </main>
+
+      <Footer />
+    </>
+  )
+}
---
+++ app/terms-of-use/page.tsx
@@ -0,0 +1,61 @@
+import type { Metadata } from 'next'
+import { Header } from '@/components/Header/Header'
+import { Footer } from '@/components/Footer/Footer'
+
+export const metadata: Metadata = {
+  title: 'My Page Title',
+}
+
+
+export default function TermsOfUse() {
+  return (
+    <>
+      <Header />
+
+      <main className="my-20 container mx-auto px-5 md:px-10 lg:px-32">
+        <h2 className="font-bold text-white text-center text-4xl">Termos de uso</h2>
+
+        <p className="mt-10">Última atualização: 25 de março de 2025</p>
+        <h3 className="mt-10">Estes Termos de Uso regem sua utilização do aplicativo Soccer Bolão. Ao usar o aplicativo, você concorda com estes Termos.</h3>
+
+        <h3 className="font-bold mt-10 mb-3">1. Objetivo do Aplicativo</h3>
+        <p className="">O Soccer Bolão é um aplicativo de entretenimento, permitindo que usuários criem bolões e ligas de futebol para fazer palpites e se divertir com amigos ou outros participantes. O aplicativo não é uma plataforma de apostas e não tem como objetivo envolver dinheiro ou apostas financeiras.</p>
+
+        <h3 className="font-bold mt-10 mb-3">2. Cadastro e Conta do Usuário</h3>
+        <p className="">Para usar o aplicativo, você precisa criar uma conta com as seguintes informações:</p>
+        <p className="ml-5">- Nome completo</p>
+        <p className="ml-5">- Email</p>
+        <p className="ml-5">- Foto de perfil (opcional)</p>
+        <p className="ml-5">- Nome de usuário</p>
+        <p className="">É sua responsabilidade manter a segurança e a confidencialidade dos seus dados de login. Não nos responsabilizamos por qualquer uso não autorizado de sua conta.</p>
+
+        <h3 className="font-bold mt-10 mb-3">3. Assinatura Premium</h3>
+        <p className="">O Soccer Bolão oferece uma assinatura premium para recursos exclusivos. A gestão de pagamentos e a coleta de informações financeiras, como dados de cartão de crédito, são realizadas exclusivamente através do Google Payments. Não coletamos nem armazenamos informações financeiras de nossos usuários.</p>
+
+        <h3 className="font-bold mt-10 mb-3">4. Exibição de Anúncios</h3>
+        <p className="">O aplicativo pode exibir anúncios fornecidos por terceiros. Esses anúncios não são de responsabilidade do Soccer Bolão, e a coleta de dados relacionados à exibição desses anúncios é regida pelas políticas de privacidade e termos de uso dos fornecedores de anúncios terceiros. Não somos responsáveis pelo conteúdo ou pela precisão dos anúncios exibidos.</p>
+
+        <h3 className="font-bold mt-10 mb-3">5. Comportamento do Usuário</h3>
+        <p className="">Ao utilizar o aplicativo, você concorda em não:</p>
+        <p className="ml-5">- Utilizar o aplicativo para fins ilegais ou fraudulentos;</p>
+        <p className="ml-5">- Promover conteúdo ofensivo, abusivo ou prejudicial;</p>
+        <p className="ml-5">- Violar os direitos de propriedade intelectual de outros usuários ou de terceiros;</p>
+        <p className="">Reservamo-nos o direito de suspender ou banir qualquer usuário que violar estes Termos de Uso.</p>
+
+        <h3 className="font-bold mt-10 mb-3">6. Propriedade Intelectual</h3>
+        <p className="">O conteúdo, design, código e funcionalidades do aplicativo Soccer Bolão são de propriedade exclusiva do desenvolvedor e são protegidos por direitos autorais e outras leis de propriedade intelectual.</p>
+
+        <h3 className="font-bold mt-10 mb-3">7. Limitação de Responsabilidade</h3>
+        <p className="">Não nos responsabilizamos por danos indiretos, incidentais, especiais ou consequenciais decorrentes do uso ou da impossibilidade de uso do aplicativo. O aplicativo é fornecido como está, sem garantias expressas ou implícitas.</p>
+
+        <h3 className="font-bold mt-10 mb-3">8. Alterações nos Termos de Uso</h3>
+        <p className="">Podemos atualizar estes Termos de Uso a qualquer momento. Qualquer alteração será publicada nesta página e, caso necessário, será comunicada a você através do seu e-mail ou por meio de notificações no aplicativo.</p>
+
+        <h3 className="font-bold mt-10 mb-3">9. Legislação Aplicável</h3>
+        <p className="">Estes Termos de Uso serão regidos e interpretados de acordo com as leis do Brasil. Qualquer disputa será resolvida nos tribunais competentes.</p>
+      </main>
+
+      <Footer />
+    </>
+  )
+}
